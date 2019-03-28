60deaf0bb3dc3aefbc69843cda3cee091c5e4748
petrspelos <petr.spelos@gmail.com>
2019-03-28 11:09:08 +0100

    Add Alice

--- main.pl
+++ main.pl
@@ -1,12 +1,11 @@
 person(peter).
 person(senne).
 person(draxis).
+person(alice).
 
 % pairs
-knows(peter, senne).
-knows(senne, peter).
 knows(peter, draxis).
 knows(draxis, peter).
-knows(senne, draxis).
-knows(draxis, senne).
+knows(alice, draxis).
+knows(draxis, alice).
 
